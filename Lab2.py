dd458b71abe55a2e7fa8fc68ad5b07bf217c1e3a
Ryan <ryan.21@ichat.sp.edu.sg>
2022-04-27 14:25:12 +0800

    Invented magic

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,42 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas: ")
+    return 0
+
+def get_user_input():
+    datalist = [int(x) for x in input().split(',')]
+    return datalist
+
+def calc_average(datalist):
+    mean = sum(datalist)/len(datalist)
+    print("Average number is ",mean)
+    return mean
+
+def find_min_max(datalist):
+    min = max = datalist[0]
+    i = 0
+    for i in range(0,len(datalist)):
+        if max < datalist[i]:
+            max = datalist[i]
+        elif min > datalist[i]:
+            min = datalist[i]
+    int_list = [min, max]
+    print("Minimum and maximum number are ",int_list)
+    return 0
+
+def sort_temperature():
+    print("sort_temperature")
+    return 0
+
+def calc_median_temperature():
+    print("calc_median_temperature")
+    return 0
+
+def main():
+    display_main_menu()
+    datalist = get_user_input()
+    calc_average(datalist)
+    find_min_max(datalist)
+    sort_temperature(datalist)
+    calc_median_temperature(datalist)
+if __name__ == '__main__':
+    main()
